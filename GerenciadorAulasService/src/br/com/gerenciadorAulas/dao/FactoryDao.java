90aa4cac3222e56002561a394c1a44a97989266e
luiz-bonato-it <luiz.bonato.it@gmail.com>
2017-09-29 00:18:52 -0300

    Commit nobvos serviços

--- GerenciadorAulasService/src/br/com/gerenciadorAulas/dao/FactoryDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.gerenciadorAulas.dao;
-
-import br.com.gerenciadorAulas.entity.Usuario;
-
-public class FactoryDao {
-	public static InterfaceDao<Usuario> getUsuarioDao(){
-		return  new UsuarioDao();
-	}
-}
